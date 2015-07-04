78c6a1656c16d93187a378499eb34e6583945162
Suyeol Jeon <devxoul@gmail.com>
2015-07-04 21:13:04 +0900

    Add test for `swift_seedname_prefix`.

--- test/test_core.rb
+++ test/test_core.rb
@@ -250,4 +250,24 @@ class CoreTest < Minitest::Test
     assert_equal uuids_before, uuids_after
   end
 
+  def test_swift_seedname_prefix
+    seedfile %{
+      swift_seedname_prefix!
+      github "devxoul/JLToast", "1.2.2", :files => "JLToast/*.{h,swift}"
+    }
+    @seed.install
+
+    self.project["Seeds"]["JLToast"].files.each do |file|
+      assert_match /(.*\.h|JLToast_.*\.swift)/, file.name
+    end
+
+    self.project.targets.each do |target|
+      phase = target.sources_build_phase
+      assert phase.files.length > 0
+      phase.file_display_names.each do |filename|
+        assert_match /(.*\.h|JLToast_.*\.swift)/, filename
+      end
+    end
+  end
+
 end
