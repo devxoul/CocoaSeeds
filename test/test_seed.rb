2fe460e033c375791e3d3d0927925847963d4307
Suyeol Jeon <devxoul@gmail.com>
2015-10-17 04:53:19 +0900

    Add test code for git commit.

--- test/test_seed.rb
+++ test/test_seed.rb
@@ -15,6 +15,20 @@ class SeedTest < Test
   end
 
 
+  def test_github_commit
+    seedfile %{
+      github "devxoul/JLToast", :commit => "83a1b50153ed26f0ae0e90d65"
+    }
+    @seed.prepare_requirements
+    @seed.execute_seedfile
+    s = @seed.seeds["JLToast"]
+    assert_equal s.name, "JLToast"
+    assert_nil s.version
+    assert_equal s.commit, "83a1b50"
+    assert_equal s.url, "https://github.com/devxoul/JLToast"
+  end
+
+
   def test_github_version_and_files
     seedfile %{
       github "devxoul/JLToast", "1.3.2", :files => "JLToast/*.{h,swift}"
@@ -44,6 +58,27 @@ class SeedTest < Test
   end
 
 
+  def test_github_commit_and_files
+    seedfile %{
+      github "devxoul/JLToast",
+        :commit => "83a1b50",
+        :files => "JLToast/*.{h,swift}"
+    }
+    @seed.prepare_requirements
+    @seed.execute_seedfile
+    s = @seed.seeds["JLToast"]
+    assert_equal s.name, "JLToast"
+    assert_nil s.version
+    assert_equal s.commit, "83a1b50"
+    assert_equal s.url, "https://github.com/devxoul/JLToast"
+    assert_equal s.files, ["JLToast/*.{h,swift}"]
+  end
+
+end
+
+
+class SeedTest
+
   def test_bitbucket_version
     seedfile %{
       bitbucket "devxoul/JLToast", "1.3.2"
@@ -54,6 +89,20 @@ class SeedTest < Test
     assert_equal s.name, "JLToast"
     assert_equal s.version, "1.3.2"
     assert_equal s.url, "https://bitbucket.org/devxoul/JLToast"
+  end
+
+
+  def test_bitbucket_commit
+    seedfile %{
+      github "devxoul/JLToast", :commit => "83a1b50"
+    }
+    @seed.prepare_requirements
+    @seed.execute_seedfile
+    s = @seed.seeds["JLToast"]
+    assert_equal s.name, "JLToast"
+    assert_nil s.version
+    assert_equal s.commit, "83a1b50"
+    assert_equal s.url, "https://github.com/devxoul/JLToast"
   end
 
 
@@ -83,6 +132,23 @@ class SeedTest < Test
     assert_equal s.version, "1.3.2"
     assert_equal s.url, "https://bitbucket.org/devxoul/JLToast"
     assert_equal s.files, ["JLToast/JLToast.h", "JLToast/JLToast.swift"]
+  end
+
+
+  def test_bitbucket_commit_and_files
+    seedfile %{
+      github "devxoul/JLToast",
+        :commit => "83a1b50",
+        :files => "JLToast/*.{h,swift}"
+    }
+    @seed.prepare_requirements
+    @seed.execute_seedfile
+    s = @seed.seeds["JLToast"]
+    assert_equal s.name, "JLToast"
+    assert_nil s.version
+    assert_equal s.commit, "83a1b50"
+    assert_equal s.url, "https://github.com/devxoul/JLToast"
+    assert_equal s.files, ["JLToast/*.{h,swift}"]
   end
 
 end
