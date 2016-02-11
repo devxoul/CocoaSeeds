6fc0264f7be8036709ba0d1462d07a784cd90bd1
Veight Zhou <veightz@gmail.com>
2016-02-11 18:28:21 +0800

    Add a new suffix of source files.

--- lib/cocoaseeds/core.rb
+++ lib/cocoaseeds/core.rb
@@ -603,7 +603,7 @@ module Seeds
     # @!visibility private
     #
     def valid_source_file? filename
-      suffixs = [".h", ".c", ".m", ".mm", ".swift"]
+      suffixs = [".h", ".c", ".m", ".mm", ".swift", ".cpp"]
       suffixs.each do |suffix|
         return true if filename.name.end_with? suffix
       end
